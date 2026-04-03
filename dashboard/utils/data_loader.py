5c45d81166e45fd5dbf4363435f341ea2363239e
Ridhisha Tyagi <ridhisha.tyagi20@gmail.com>
2026-04-03 21:47:59 +0530

    fixed base path to dashboard

--- dashboard/utils/data_loader.py
+++ dashboard/utils/data_loader.py
@@ -1,13 +1,13 @@
 import os
 import pandas as pd
 
-# Base path = dashboard folder
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# Get dashboard folder directly
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 def load_data(filename):
     file_path = os.path.join(BASE_DIR, "data", filename)
-    
+
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"{filename} not found at {file_path}")
-    
+
     return pd.read_csv(file_path)
